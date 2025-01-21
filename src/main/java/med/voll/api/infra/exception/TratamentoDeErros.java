8b5ea1394e87e689089560a108ca2c32fa927726
Filipe Ferreira <filipeferreira.psid2019@gmail.com>
2025-01-21 11:43:06 -0300

    Novos testes automatizados

--- src/main/java/med/voll/api/infra/exception/TratamentoDeErros.java
+++ src/main/java/med/voll/api/infra/exception/TratamentoDeErros.java
@@ -1,12 +1,19 @@
 package med.voll.api.infra.exception;
 
 import jakarta.persistence.EntityNotFoundException;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @RestControllerAdvice
 public class TratamentoDeErros {
@@ -15,22 +22,24 @@ public class TratamentoDeErros {
     public ResponseEntity exceptionNotFound(){
         return ResponseEntity.notFound().build();
     }
-
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity badRequest(MethodArgumentNotValidException ex){
-        var errors = ex.getFieldErrors();
-        return ResponseEntity.badRequest().body(errors.stream().map(ErrorDataValidation::new).toList());
-    }
+    public ResponseEntity<Map<String, List<String>>> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        List<String> errors = ex.getBindingResult().getFieldErrors().stream().map(FieldError::getDefaultMessage)
+                .collect(Collectors.toList());
 
-    private record ErrorDataValidation(String field, String message){
-        public ErrorDataValidation(FieldError error){
-            this(error.getField(), error.getDefaultMessage());
+        return new ResponseEntity<>(getErrorsMap(errors), new HttpHeaders(),HttpStatus.BAD_REQUEST);
         }
-    }
+
+        private Map<String, List<String>> getErrorsMap(List<String> errors) {
+            Map<String, List<String>> errorsResponse = new HashMap<>();
+            errorsResponse.put("errors", errors);
+            return errorsResponse;
+        }
 
     @ExceptionHandler(ValidacaoException.class)
     public ResponseEntity businessRules (ValidacaoException ex){
-        var error = new ErrorDataValidation(ex.getFieldName(), ex.getMessage());
+        var error = new ValidacaoException(ex.getFieldName(), ex.getMessage());
         return ResponseEntity.badRequest().body(error);
     }
 
--- src/test/java/med/voll/api/domain/repository/MedicoRepositoryTest.java
+++ src/test/java/med/voll/api/domain/repository/MedicoRepositoryTest.java
@@ -1,8 +1,12 @@
 package med.voll.api.domain.repository;
 
+import med.voll.api.domain.dto.DadosCadastroDTO;
 import med.voll.api.domain.dto.EnderecoDTO;
 import med.voll.api.domain.dto.PacienteDadosCadastroDTO;
+import med.voll.api.domain.model.Consulta;
 import med.voll.api.domain.model.Especialidade;
+import med.voll.api.domain.model.Medico;
+import med.voll.api.domain.model.Paciente;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,6 +17,7 @@ import org.springframework.test.context.ActiveProfiles;
 
 import java.time.DayOfWeek;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.temporal.TemporalAdjuster;
 import java.time.temporal.TemporalAdjusters;
 
@@ -33,13 +38,54 @@ class MedicoRepositoryTest {
     @Test
     @DisplayName("Deveria devolver null quando o unico medico cadastrado não está disponivel na data")
     void escolherMedicoAleatorioLivreNaDataCenario1() {
+        //given or arrange
+        var nextMonday10Am = LocalDate.now().with(TemporalAdjusters.next(DayOfWeek.MONDAY)).atTime(10,0);
 
+
+        var medico = cadastrarMedico("Medico 1", "medico@voll.med", "123456", Especialidade.CARDIOLOGIA);
+        var paciente = cadastrarPaciente("Paciente", "paciente@gmail.com","12345678910");
+        cadastrarConsulta(medico,paciente,nextMonday10Am);
+
+        //when or act
+        var freeDoctor = medicoRepository.escolherMedicoAleatorioLivreNaData(Especialidade.CARDIOLOGIA,nextMonday10Am);
+
+        //then or assert
+        assertThat(freeDoctor).isNull();
+
+    }
+
+    @Test
+    @DisplayName("Deveria devolver medico quando ele estiver disponivel na data")
+    void escolherMedicoAleatorioLivreNaDataCenario2() {
 
         var nextMonday10Am = LocalDate.now().with(TemporalAdjusters.next(DayOfWeek.MONDAY)).atTime(10,0);
 
-        var freeDoctor = medicoRepository.escolherMedicoAleatorioLivreNaData(Especialidade.CARDIOLOGIA,nextMonday10Am);
-        assertThat(freeDoctor).isNull();
+        var medico = cadastrarMedico("Medico 1", "medico@voll.med", "123456", Especialidade.CARDIOLOGIA);
 
+        var freeDoctor = medicoRepository.escolherMedicoAleatorioLivreNaData(Especialidade.CARDIOLOGIA,nextMonday10Am);
+        assertThat(freeDoctor).isEqualTo(medico);
+
+    }
+
+
+    private void cadastrarConsulta(Medico medico, Paciente paciente, LocalDateTime dateTime){
+        em.persist(new Consulta(null,medico,paciente,dateTime));
+    }
+
+    private Paciente cadastrarPaciente(String nome, String email, String cpf){
+        var paciente = new Paciente(dataPaciente(nome,email,cpf));
+        em.persist(paciente);
+        return paciente;
+    }
+
+    private Medico cadastrarMedico(String nome, String email, String crm, Especialidade especialidade){
+        var medico = new Medico(dadosMedico(nome,email,crm,especialidade));
+        em.persist(medico);
+        return medico;
+    }
+
+    private DadosCadastroDTO dadosMedico(String nome, String email, String crm, Especialidade especialidade){
+        return new DadosCadastroDTO(nome,email,"123456",especialidade,crm,dadosEndereco());
     }
 
     private PacienteDadosCadastroDTO dataPaciente(String nome, String email, String cpf){
